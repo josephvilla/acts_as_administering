7e26fbaab5f755c8151694c53478070583e61cc3
Joseph Villa <joseph@bigbeelabs.com>
2020-07-22 12:35:45 -0700

    HM-135 #comment << administrator #time 1h

--- lib/acts_as_administrated_by/define_methods/administrators.rb
+++ lib/acts_as_administrated_by/define_methods/administrators.rb
@@ -5,23 +5,41 @@ module ActsAsAdministratedBy
       module AdministratorsMethodProxy
 
         def <<(administrator)
-          raise "in #{@__self__.class.name}.#{__method__}, expected a #{@expected_class}, but got a #{administrator.class.name}" unless administrator.is_a?(@expected_class)
-          relate_method = "relate_to_#{@__self__.class.name.underscore}"
-          administrator.respond_to?(relate_method).tap do |responds| 
-            raise "#{administrator.class.name} does not administer #{@__self__.class.name.pluralize}" unless responds
-            administrator.send(relate_method, @__self__, as: 'admin')
+          raise "in #{@__self__.class.name}.#{__method__}, expected a #{@__expected_class__}, but got a #{administrator.class.name}" unless administrator.is_a?(@__expected_class__)
+          if @__as__
+            relate_method = "administrated_#{@__as__.to_s.pluralize}"
+          else
+            relate_method = "administrated_#{@__self__.class.name.demodulize.underscore.pluralize}"
+          end
+          administrator.respond_to?(relate_method).tap do |responds|
+            raise "#{administrator.class.name} does not administrate #{@__self__.class.name.pluralize}".red unless responds
+            administrator.send(relate_method).send(:<<, @__self__)
           end
         end
 
         def remove(administrator)
-          raise "in #{@__self__.class.name}.#{__method__}, expected a #{@expected_class}, but got a #{administrator.class.name}" unless administrator.is_a?(@expected_class)          
+          raise "in #{@__self__.class.name}.#{__method__}, expected a #{@__expected_class__}, but got a #{administrator.class.name}" unless administrator.is_a?(@__expected_class__)          
           stop_relating_method = "stop_relating_to_#{@__self__.class.name.underscore}"
           administrator.respond_to?(stop_relating_method).tap do |responds|
-            raise "#{administrator.class.name} does not administer #{@__self__.class.name.pluralize}" unless responds
+            raise "#{administrator.class.name} does not administrate #{@__self__.class.name.pluralize}" unless responds
             administrator.send(stop_relating_method, @__self__, as: 'admin')
           end
         end
+
+        def ids
+          self.map(&:id)
+        end
         
+      end
+
+      module AdminsIncludeProxy
+        def include?(args)
+          args_ids = [args] if args.is_a?(Integer)
+          args_ids ||= args if (args_ids.blank? && args.is_a?(Array) && args.first.is_a?(Integer))
+          args_ids ||= [args.id] if (args_ids.blank? && args.respond_to?(:id))
+          args_ids ||= args.map(&:id) if (args_ids.blank? && args.first.respond_to?(:id))
+          !(self.ids & args_ids).blank?
+        end
       end
 
       def define_method_administrators(class_sym, options)
@@ -36,8 +54,11 @@ module ActsAsAdministratedBy
             send("#{class_sym}_that_relate_to_me", as: 'admin').tap do |administrators|
               return administrators if administrators.is_a?(AdministratorsMethodProxy)
               administrators.extend(AdministratorsMethodProxy)
-              administrators.instance_variable_set(:@expected_class, expected_class)
+              administrators.extend(AdminsIncludeProxy) if administrators.is_a?(Array)
+              administrators.instance_variable_set(:@__expected_class__, expected_class)
               administrators.instance_variable_set(:@__self__, self)
+              administrators.instance_variable_set(:@__as__, options[:as])
+              administrators.instance_variable_set(:@__class_sym__, class_sym)
             end
           end
         end
